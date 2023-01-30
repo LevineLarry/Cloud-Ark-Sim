33c961bda1a3d2d4f8dc3eb1e7f2865b0651cf7c
LevineLarry <larrylevine312@gmail.com>
2023-01-29 21:06:01 -0500

    Demo is operational. Broadcasts orientation over websocket connection

---
+++ Cloud Ark Sim/lib/IPC/SocketServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Cloud_Ark_Sim.lib.IPC
+{
+    class SocketServer
+    {
+        public static WebSocketServer wssv;
+        public static void Start()
+        {
+            wssv = new("ws://localhost:1234");
+
+            wssv.AddWebSocketService<SendData>("/Data");
+            wssv.Start();
+        }
+    }
+
+    public class SendData : WebSocketBehavior
+    {
+        protected override void OnMessage (MessageEventArgs e)
+        {
+            
+        }
+
+        public void Send(String data)
+        {
+            Sessions.Broadcast(data);
+        }
+    }
+}
